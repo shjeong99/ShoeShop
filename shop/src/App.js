e5098b58a8c30f90d4029d21ba8a5390cfdeb3f5
DESKTOP-IO9TGIJ\정서형 <shjeong99>
2024-03-09 20:59:13 +0900

    리액트 라우터

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,13 +1,15 @@
 /* eslint-disable */ //warning 없애기
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
-import './App.css';
-import LogoImg from './img/bg.png'
-import { useState } from 'react';
-import data from './data.js';
+import "./App.css";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import LogoImg from "./img/bg.png";
+import { useState } from "react";
+import data from "./data.js";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./pages/Detail.js";
 
 function App() {
-
   let [shoes, setShoes] = useState(data);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -15,41 +17,93 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Cart
+            </Nav.Link>
+            {/* <Link to="/" className="navTab">
+              메인
+            </Link>
+            <Link to="/detail" className="navTab">
+              상세페이지
+            </Link> */}
           </Nav>
         </Container>
       </Navbar>
 
-      <div className="main-bg" style={{ backgroundImage : 'url(' + LogoImg + ')'}}></div>
-
-      <div className="container">
-        <div className="row">
-          {/* <div className="col-md-4">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div
+                className="main-bg"
+                style={{ backgroundImage: "url(" + LogoImg + ")" }}
+              ></div>
+              <div className="container">
+                <div className="row">
+                  {/* <div className="col-md-4">
             <img src={process.env.PUBLIC_URL + "/shoes1.jpg"} width="80%"/>
             <h4>{shoes[0].title}</h4>
             <p>{shoes[0].price}</p>
           </div> */}
-          {
-            shoes.map(function(val, idx){
-              return(
-                <ShoesList shoes={shoes[idx]} img={"https://codingapple1.github.io/shop/shoes" + (idx + 1) + ".jpg"}/>
-              );
-            })
+                  {shoes.map(function (val, idx) {
+                    return (
+                      <Card
+                        shoes={shoes[idx]}
+                        img={
+                          "https://codingapple1.github.io/shop/shoes" +
+                          (idx + 1) +
+                          ".jpg"
+                        }
+                        key={idx}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            </>
           }
-        </div>
-      </div>
+        />
+        <Route path="/detail" element={<Detail />} />
 
+        {/* nested routes */}
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>} />
+          <Route path="location" element={<div>위치정보임</div>} />
+        </Route>
+
+        <Route path="*" element={<div>없는 페이지</div>} />
+      </Routes>
     </div>
   );
 }
 
-function ShoesList(props){
-  return(
+function About() {
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
     <div className="col-md-4">
-      <img src={props.img} width="80%"/>
-        <h4>{props.shoes.title}</h4>
-          <p>{props.shoes.price}원</p>
+      <img src={props.img} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}원</p>
     </div>
   );
 }
