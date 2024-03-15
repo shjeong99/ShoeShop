0c22e217a089c7a4a96d2116dd658b25c5989ded
shjeong99 <mcncshjeong@gmail.com>
2024-03-15 15:43:17 +0900

    useEffect

--- shop/src/pages/Detail.js
+++ shop/src/pages/Detail.js
@@ -1,34 +1,45 @@
 /* eslint-disable */ //warning 없애기
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
-
-let YellowBtn = styled.button`
-  background : ${ props => props.bg };
-  color : ${ props => props.bg == 'blue' ? 'white' : 'black'};
-  padding : 10px;
-`
-let Box = styled.div`
-  background : grey;
-  padding : 20px;
-`
-// 기존 스타일 복사
-let NewBtn = styled.button(YellowBtn) `
-`
+// import styled from "styled-components";
 
 function Detail(props) {
+
+  let [alertYN, setAlertYN] = useState(true); 
+  let [count, setCount] = useState(0);
   
   let {id} = useParams();
-  console.log(id);
-
   let shoesOne = props.shoes.find((val) => val.id == id);
-  console.log(shoesOne);
 
+  useEffect(()=>{ //mount, update시 코드 실행해주는 useEffect (근데 useEffect에 굳이 안넣어도 똑같음)
+    //다른점. useEffect 안에 있는 코드는 html 렌더링 후에 동작함
+    //side Effect 코드(함수의 핵심기능과 상관없는 부가기능) 보관함
+    let timer = setTimeout(() => { setAlertYN(false); }, 2000); 
+
+    return () => { //useEffect 동작 전에 실행됨
+      clearTimeout(timer);
+      console.log(1);
+      //clean up function은 mount시 실행 안됨,
+      //unmount시 실행됨
+    }
+  }, [count]); //[]를 넣으면 []안에 state가 변할때마다 실행됨.(mount될때, 혹은 []안 state가 update될때)
+              // []안에 아무것도 없으면 mount될때만 실행됨.
+
+  let [inpVal, setInpVal] = useState("");
+  useEffect(()=>{
+    setInpVal("");
+  }, [inpVal]);
   return (
     <div className="container">
-      <Box>
-      <YellowBtn bg="blue">버튼</YellowBtn>
-      <YellowBtn bg="orange">버튼</YellowBtn>
-      </Box>
+      {
+        alertYN == true ? 
+          <div className="alert alert-warning">
+          2초이내 구매시 할인
+          </div> 
+          : null
+      }
+      {count}
+      <button onClick={()=>{ setCount(count+1)}}>버튼</button>
       <div className="row">
         <div className="col-md-6">
           <img
@@ -36,6 +47,20 @@ function Detail(props) {
             alt="detailImg"
             width="100%"
           />
+        </div>
+        <div style={{textAlign:"center"}}>
+          <input type="tel" style={{width:"100px", textAlign:"center"}} onChange={(e)=>{
+            var inpVal = e.target.value;
+            var replaceNotInt = /[^0-9]/gi; // 숫자가 아닌 정규식
+            console.log(this);
+            console.log()
+            if (inpVal.match(replaceNotInt)) {
+              alert("숫자만 입력 가능합니다");
+              // val.replace(replaceNotInt, "");
+              setInpVal("");
+              e.target.value = "";
+            }
+          }}></input>
         </div>
         <div className="col-md-6">
           <h4 className="pt-5">{shoesOne.title}</h4>
